9e9468b8600b47b44dd068fbdc7b6f745becbda2
Eren-Unal <bilalunal24@gmail.com>
2024-09-27 22:43:44 +0300

    Initial Commit

---
+++ BilalErenUnal_InnovaBootcamp_Odev5Tests/BilalErenUnal_InnovaBootcamp_Odev5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BilalErenUnal_InnovaBootcamp_Odev5Tests.swift
+//  BilalErenUnal_InnovaBootcamp_Odev5Tests
+//
+//  Created by Eren on 27.09.2024.
+//
+
+import Testing
+@testable import BilalErenUnal_InnovaBootcamp_Odev5
+
+struct BilalErenUnal_InnovaBootcamp_Odev5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
